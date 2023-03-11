81791b1c2de098cf88569d9911727d6ca061d7e0
Andy Castles <andy@andrewc.com>
2023-03-11 06:18:19 +0100

    Added support for excluding letters in specific positions

--- index.mjs
+++ index.mjs
@@ -1,16 +1,24 @@
 import getopts from 'getopts'
 
 const letters = [
-	'A', 'E', 'I', 'O', 'U',
-	'B', 'C', 'D', 'F', 'G', 'H', 'J',
-	'K', 'L', 'M', 'N', 'P', 'Q', 'R',
-	'S', 'T', 'V', 'W', 'X', 'Y', 'Z'
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+	'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+	'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+//	'A', 'E', 'I', 'O', 'U',
+//	'B', 'C', 'D', 'F', 'G', 'H', 'J',
+//	'K', 'L', 'M', 'N', 'P', 'Q', 'R',
+//	'S', 'T', 'V', 'W', 'X', 'Y', 'Z'
 ];
 const supportedOptions = {
     alias: {
         help: ['h'],
         skip: ['s'],
-        require: ['r']
+        require: ['r'],
+	first: ['1'],
+	second: ['2'],
+	third: ['3'],
+	fourth: ['4'],
+	fifth: ['5']
     },
     help: {
         help: 'Show this help',
@@ -19,20 +27,42 @@ const supportedOptions = {
     }
 }
 
-function replaceGap(template, replacements) {
+function replaceGap(template, firstGo) {
 	if (template.indexOf('.') < 0) {
-		if (require.every(letter => template.indexOf(letter) >= 0)) {
+		let t = template
+		if (require.every(letter => {
+			if (t.indexOf(letter) < 0) {
+				return false
+			} else {
+				t = t.replace(letter,'')
+				return true
+			}
+		})) {
 			line += template + ' '
 		} else {
 		}
+//console.log(t)
+		if (firstGo) console.log(line)
 		return
 	}
 	for(let letter of letters) {
 		if (skip.indexOf(letter) < 0) {
-			replaceGap(template.replace('.', letter), replacements + 1)
-			if (replacements === 0) {
-				if (line) console.log(line)
-				line = ''
+			let position = template.indexOf('.')
+			let skipPosition = []
+			switch(position) {
+				case 0: skipPosition = first; break;
+				case 1: skipPosition = second; break;
+				case 2: skipPosition = third; break;
+				case 3: skipPosition = fourth; break;
+				case 4: skipPosition = fifth; break;
+			}
+//console.log(skipPosition, letter, skipPosition)
+			if (skipPosition.indexOf(letter) < 0) {
+				replaceGap(template.replace('.', letter))
+				if (firstGo) {
+					if (line) console.log(line)
+					line = ''
+				}
 			}
 		}
 	}
@@ -69,6 +99,7 @@ const options = getopts(process.argv.slice(2), supportedOptions)
 const invalidSwitches = invalidArgs(options, supportedOptions)
 
 let template, require, skip, line
+let first, second, third, fourth, fifth
 
 if (options.help) {
     help()
@@ -85,15 +116,25 @@ if (options.help) {
     template = options._[0].toUpperCase()
     require = (options.require || '').toUpperCase().split('')
     skip = (options.skip || '').toUpperCase().split('')
+    first = (options.first || '').toUpperCase().split('')
+    second = (options.second || '').toUpperCase().split('')
+    third = (options.third || '').toUpperCase().split('')
+    fourth = (options.fourth || '').toUpperCase().split('')
+    fifth = (options.fifth || '').toUpperCase().split('')
 
     console.log('Template: ', template)
     console.log('Require:  ',require.join(' ') || '(no required letters)')
     console.log('Skip:     ', skip.join(' ') || '(no skipped letters)')
+    if (first.length > 0) console.log('Cannot be in position 1:     ', first.join(' '))
+    if (second.length > 0) console.log('Cannot be in position 2:     ', second.join(' '))
+    if (third.length > 0) console.log('Cannot be in position 3:     ', third.join(' '))
+    if (fourth.length > 0) console.log('Cannot be in position 4:     ', fourth.join(' '))
+    if (fifth.length > 0) console.log('Cannot be in position 5:     ', fifth.join(' '))
 
     line = ''
 
     console.log('\nPossible words:')
-    replaceGap(template, 0)
+    replaceGap(template, true)
 }
 
 
